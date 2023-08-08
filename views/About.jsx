4e3d942bc58a4d897bf6b0c9af58bd753946797d
Zachary Kuchar <zacharyk323@live.com>
2023-08-08 13:44:46 -0400

    Update with Node modules and other changes

---
+++ views/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1>You can find my favorite things on this site</h1>
+      <h1>Simply use a number in the params</h1>
+    </div>
+  )
+}
+
+export default About
